4888f3f407d506dd243e9d68c45a0f0446075ed7
19007457 <19007457@myrp.edu.sg>
2021-04-20 11:55:58 +0800

    Second Commit

--- app/src/main/java/sg/edu/rp/c346/julien/demolistview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/julien/demolistview/MainActivity.java
@@ -2,8 +2,12 @@ package sg.edu.rp.c346.julien.demolistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         aa = new FoodAdapter(this, R.layout.row, food);
         lv.setAdapter(aa);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Food selectedFood = food.get(position);
+                Toast.makeText(MainActivity.this, selectedFood.getName() + " Star: " + selectedFood.isStar(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
